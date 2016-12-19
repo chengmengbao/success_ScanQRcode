3c9fafe8963dcc3432e983920c3042b8f9da0882
chengmengbao <paul_chengmengbao@163.com>
2016-12-19 05:19:29 +0000

    按操作说明操作

--- main.py
+++ main.py
@@ -7,18 +7,40 @@ import pic
 import parse
 import time
 
-i = 0
+i = 1
 
-while(1):
+while True:
     
     main_t1 = time.time()
 
-    pic.take_pic(i)
-    parse.lesen(i)
+    input_str = raw_input("输入命令：")
 
-    i = i+1
+    if ( 'c' == input_str ):
+        pic.take_pic(i)
+        parse.lesen(i)
+        i += 1
+        print "#############################################"
 
+    elif ( 'q' == input_str ):
+        print "结束"
+        break
+
+    elif ( 'c -n 100' == input_str ):
+        for n in range(10):
+            pic.take_pic(i)
+            parse.lesen(i)
+            i += 1
+            print "#############################################"
+
+    elif ( 'c -n 50' == input_str ):
+        for n in range(5):
+            pic.take_pic(i)
+            parse.lesen(i)
+            i += 1
+            print "#############################################"
+    else:
+        print "输入命令错误！！！"
     main_t2 = time.time()
-    print "main_t2-main_t1=%f" % (main_t2-main_t1)
+    # print "主程序运行时间=%f" % (main_t2-main_t1)
 
-    print "#############################################"
+
