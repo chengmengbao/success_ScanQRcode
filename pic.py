3573734b4ea215330d97d702ad337640b8dfec31
chengmengbao <paul_chengmengbao@163.com>
2016-12-19 05:26:59 +0000

    按操作说明操-pic.py

--- pic.py
+++ pic.py
@@ -6,8 +6,15 @@
 import picamera
 import time
 
+# 摄像头初始化
 camera = picamera.PiCamera()
-# camera.resolution = (360, 240)
+# camera.resolution = (640, 480)
+camera.framerate = 30
+# camera.annotate_text = "HKUTANGYU.Inc"
+# 垂直翻转
+camera.vflip = True
+# 水平翻转
+camera.hflip = True
 camera.start_preview()
 time.sleep(2)
 
@@ -20,4 +27,4 @@ def take_pic(i):
     print "拍照完成，第%d张" % i
 
     pic_t2 = time.time()
-    print "拍照花费时间=%f" % (pic_t2-pic_t1)
+    # print "拍照花费时间=%f" % (pic_t2-pic_t1)
